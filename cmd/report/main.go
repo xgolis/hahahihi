4eb635ce1aabaf62568e161e542428323a8836d6
xgolis <xgolis@stuba.sk>
2023-07-02 14:30:57 +0200

    bumbac

--- cmd/report/main.go
+++ cmd/report/main.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 	"os"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/xuri/excelize/v2"
@@ -21,8 +20,9 @@ type App struct {
 	Auth    string
 	ICG     int
 	Values  []float64
-	Dates   []time.Time
+	Dates   []string
 	Month   int
+	Daily   bool
 }
 
 type Data struct {
@@ -32,8 +32,11 @@ type Data struct {
 
 type Values struct {
 	Date string  `json:"datef"`
+	IC01 float64 `json:"ic1,omitempty"`
 	IC02 float64 `json:"ic2,omitempty"`
+	IC03 float64 `json:"ic3,omitempty"`
 	IC04 float64 `json:"ic4,omitempty"`
+	IC05 float64 `json:"ic5,omitempty"`
 	IC06 float64 `json:"ic6,omitempty"`
 	IC07 float64 `json:"ic7,omitempty"`
 	IC08 float64 `json:"ic8,omitempty"`
@@ -86,6 +89,8 @@ var devices map[int]string = map[int]string{
 	1364: "RETTLH-kotolna-turbiny",
 	1355: "GOTEC-reverzna-osmoza",
 	1421: "SM62+RO-B1-3",
+	1346: "VINCENTE",
+	1435: "GPV-osmoza",
 }
 
 var icgs map[int]int = map[int]int{
@@ -93,6 +98,9 @@ var icgs map[int]int = map[int]int{
 	2: 1364,
 	3: 1355,
 	4: 1421,
+	5: 1373,
+	6: 1346,
+	7: 1435,
 }
 
 var excelInfocodes map[int][]int = map[int][]int{
@@ -100,12 +108,15 @@ var excelInfocodes map[int][]int = map[int][]int{
 	1364: {2102, 2104, 2106, 2108, 2110, 2112, 2114, 2116},
 	1355: {2102, 2104, 2106, 2108, 2110, 2112, 2114, 2116},
 	1421: {2102, 2104, 2106, 2108, 2110, 2112, 2114, 2116},
+	1346: {2001, 2002, 2003, 2004, 2005, 2006, 2007, 2011, 2012, 2013},
+	1435: {2102, 2104, 2106, 2108, 2110, 2112, 2114, 2116},
 }
 
 var alphabet = []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"}
 
-func (a *App) getUrl() string {
-	return fmt.Sprintf("%s%d?tsfrom=%d&tsto=%d", a.BaseURL, a.ICG, a.TsFrom, a.TsTo)
+func (a *App) getUrl(tsfrom, tsto int64) string {
+	fmt.Println(fmt.Sprintf("%s%d?tsfrom=%d&tsto=%d", a.BaseURL, a.ICG, tsfrom, tsto))
+	return fmt.Sprintf("%s%d?tsfrom=%d&tsto=%d", a.BaseURL, a.ICG, tsfrom, tsto)
 }
 
 func (a *App) fillArray(data Data) {
@@ -126,11 +137,13 @@ func (a *App) fillArray(data Data) {
 			a.Values = append(a.Values, value.IC22)
 			a.Values = append(a.Values, value.IC25)
 
-			formatedDate, err := time.Parse("2006-01-02", strings.Split(value.Date, " ")[0])
-			if err != nil {
-				panic(err)
-			}
-			a.Dates = append(a.Dates, formatedDate)
+			// formatedDate, err := time.Parse("2006-01-02", strings.Split(value.Date, " ")[0])
+			// if err != nil {
+			// 	panic(err)
+			// }
+			// a.Dates = append(a.Dates, formatedDate)
+			// formatedDate, _ := time.Parse("2006-01-02T15:04:05", strings.Split(value.Date, " ")[0])
+			a.Dates = append(a.Dates, value.Date)
 		}
 	} else if a.ICG == int(icgs[2]) {
 		for _, value := range data.RealData {
@@ -143,13 +156,13 @@ func (a *App) fillArray(data Data) {
 			a.Values = append(a.Values, value.IC14)
 			a.Values = append(a.Values, value.IC16)
 
-			formatedDate, err := time.Parse("2006-01-02", strings.Split(value.Date, " ")[0])
-			if err != nil {
-				panic(err)
-			}
-			a.Dates = append(a.Dates, formatedDate)
+			// formatedDate, err := time.Parse("2006-01-02", strings.Split(value.Date, " ")[0])
+			// if err != nil {
+			// 	panic(err)
+			// }
+			a.Dates = append(a.Dates, value.Date)
 		}
-	} else if a.ICG == int(icgs[3]) {
+	} else if a.ICG == int(icgs[3]) || a.ICG == int(icgs[7]) {
 		for _, value := range data.RealData {
 			a.Values = append(a.Values, value.IC02)
 			a.Values = append(a.Values, value.IC04)
@@ -160,11 +173,11 @@ func (a *App) fillArray(data Data) {
 			a.Values = append(a.Values, value.IC14)
 			a.Values = append(a.Values, value.IC16)
 
-			formatedDate, err := time.Parse("2006-01-02", strings.Split(value.Date, " ")[0])
-			if err != nil {
-				panic(err)
-			}
-			a.Dates = append(a.Dates, formatedDate)
+			// formatedDate, err := time.Parse("2006-01-02", strings.Split(value.Date, " ")[0])
+			// if err != nil {
+			// 	panic(err)
+			// }
+			a.Dates = append(a.Dates, value.Date)
 		}
 	} else if a.ICG == int(icgs[4]) {
 		for _, value := range data.RealData {
@@ -177,18 +190,40 @@ func (a *App) fillArray(data Data) {
 			a.Values = append(a.Values, value.IC14)
 			a.Values = append(a.Values, value.IC16)
 
-			formatedDate, err := time.Parse("2006-01-02", strings.Split(value.Date, " ")[0])
-			if err != nil {
-				panic(err)
-			}
-			a.Dates = append(a.Dates, formatedDate)
+			// formatedDate, err := time.Parse("2006-01-02", strings.Split(value.Date, " ")[0])
+			// if err != nil {
+			// 	panic(err)
+			// }
+			a.Dates = append(a.Dates, value.Date)
+		}
+	} else if a.ICG == int(icgs[6]) {
+		for _, value := range data.RealData {
+			a.Values = append(a.Values, value.IC01)
+			a.Values = append(a.Values, value.IC02)
+			a.Values = append(a.Values, value.IC03)
+			a.Values = append(a.Values, value.IC04)
+			a.Values = append(a.Values, value.IC05)
+			a.Values = append(a.Values, value.IC06)
+			a.Values = append(a.Values, value.IC07)
+			a.Values = append(a.Values, value.IC11)
+			a.Values = append(a.Values, value.IC12)
+			a.Values = append(a.Values, value.IC13)
+
+			a.Dates = append(a.Dates, value.Date)
 		}
 	}
 }
 
-func NewApp(tsfrom int64, tsto int64, icg int, month int) *App {
+func NewApp(tsfrom int64, tsto int64, icg int, month int, daily bool) *App {
+	var url = ""
+	if icg == 1373 || icg == 1364 || icg == 1355 || icg == 1421 || icg == 1435 {
+		url = "https://iot-api.aiwater.io/iot/data/samples/graph-series-daily/icg:"
+	} else {
+		url = "https://iot-api.aiwater.io/iot/data/samples/graph-series/icg:"
+	}
 	return &App{
-		BaseURL: "https://iot-api.aiwater.io/iot/data/samples/graph-series-daily/icg:",
+		BaseURL: url,
+		Daily:   daily,
 		Auth:    "Basic ZnJvbnRlbmQ6RzF2ZU0zRDQrNA==",
 		TsFrom:  tsfrom,
 		TsTo:    tsto,
@@ -208,15 +243,20 @@ func getMonth() int {
 	return month
 }
 
-func getICG() int {
+func getICG() (int, bool) {
 	var device int
-	fmt.Println("Select device [1-KTZLM: deminstanica, 2-RETTLH: kotolna turbiny, 3-GOTEC: reverzna osmoza, 4-SM62+RO-B1-3]")
+	fmt.Println("Select device [1-KTZLM: deminstanica, 2-RETTLH: kotolna turbiny, 3-GOTEC: reverzna osmoza, 4-SM62+RO-B1-3, 5-KTZLM: deminstanica tych vela, 6-VINCENTE, 7-GPV-rev. osmoza]")
 	_, err := fmt.Scanf("%d \n", &device)
 	if err != nil {
 		log.Panicf("error while getting value: %v", err)
 	}
 
-	return icgs[device]
+	daily := true
+	if device == 5 || device == 6 {
+		daily = false
+	}
+
+	return icgs[device], daily
 }
 
 func getTimeStamp(month int) (int64, int64) {
@@ -226,8 +266,38 @@ func getTimeStamp(month int) (int64, int64) {
 	return tsfrom, tsto
 }
 
-func (a *App) getData() Data {
-	req, err := http.NewRequest(http.MethodGet, a.getUrl(), nil)
+func getTimeStampOfDay(month int, day int, half int) (int64, int64) {
+	tsfrom := time.Date(2023, time.Month(month), day, half*12-12, 0, 0, 0, time.Now().Location()).Unix()
+	// var tsto int64
+	// if day+1 > lastDay[month] {
+	// 	tsto = time.Date(2023, time.Month(month+1), 1, 0, 0, 0, 0, time.Now().Location()).Unix()
+	// } else {
+	// 	tsto = time.Date(2023, time.Month(month), lastDay[month], 0, 0, 0, 0, time.Now().Location()).Unix()
+	// }
+	tsto := time.Date(2023, time.Month(month), day, half*12, 0, 0, 0, time.Now().Location()).Unix()
+
+	return tsfrom, tsto
+}
+
+func (a *App) noIdeaHowToNameThis() Data {
+	if a.Daily {
+		return a.getData(a.getUrl(a.TsFrom, a.TsTo))
+	}
+	var data Data
+	for i := 1; i <= lastDay[a.Month]; i++ {
+		tmpData := a.getData(a.getUrl(getTimeStampOfDay(a.Month, i, 1)))
+		data.ChartData = append(data.ChartData, tmpData.ChartData...)
+		data.RealData = append(data.RealData, tmpData.RealData...)
+		tmpData = a.getData(a.getUrl(getTimeStampOfDay(a.Month, i, 2)))
+		data.ChartData = append(data.ChartData, tmpData.ChartData...)
+		data.RealData = append(data.RealData, tmpData.RealData...)
+	}
+	return data
+
+}
+
+func (a *App) getData(url string) Data {
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		fmt.Printf("client: could not create request: %s\n", err)
 		os.Exit(1)
@@ -241,7 +311,7 @@ func (a *App) getData() Data {
 		os.Exit(1)
 	}
 
-	fmt.Printf("client: got response!\n")
+	// fmt.Printf("client: got response!\n")
 	fmt.Printf("client: status code: %d\n", res.StatusCode)
 
 	resBody, err := ioutil.ReadAll(res.Body)
@@ -283,9 +353,10 @@ func (a *App) writeExcel(data []float64) {
 	row := 2
 	for i, value := range data {
 		if i%(len(codes)) == 0 {
-			date := fmt.Sprint(a.Dates[row-2].AddDate(0, 0, 1).Format("02.01.2006"))
+			// date := fmt.Sprint(a.Dates[row-2].AddDate(0, 0, 1).Format("02.01.2006"))
+			// date := fmt.Sprint(a.Dates[row-2].AddDate(0, 0, 1).Format("02.01.2006"))
+			err = f.SetCellValue("Sheet1", "A"+strconv.Itoa(row+1), a.Dates[row-2])
 			row += 1
-			err = f.SetCellValue("Sheet1", "A"+strconv.Itoa(row), date)
 			if err != nil {
 				panic(err)
 			}
@@ -304,11 +375,14 @@ func (a *App) writeExcel(data []float64) {
 }
 
 func main() {
+	// daily := true
 	month := getMonth()
-	icg := getICG()
+	icg, daily := getICG()
 	tsfrom, tsto := getTimeStamp(month)
-	app := NewApp(tsfrom, tsto, icg, month)
-	data := app.getData()
+	app := NewApp(tsfrom, tsto, icg, month, daily)
+	fmt.Println(app)
+	data := app.noIdeaHowToNameThis()
+	// fmt.Println(data)
 	app.fillArray(data)
 	app.writeExcel(app.Values)
 }
